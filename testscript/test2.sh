181ae67c04fb9c2cfe97721d99e62c94000dbd3c
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-30 14:40:30 +0200

    Update test2.sh

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -1,39 +1,43 @@
  #!/bin/bash
  source .env
-# # Read the rootUrl value from the JSON file
-# jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+# # # Read the rootUrl value from the JSON file
+# # jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
-# # Replace the rootUrl with the one from the .env file
-# sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
-# echo "naar:"
-# jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# echo ""
-# jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# mapeditorrootUrl=$(jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+# # # Replace the rootUrl with the one from the .env file
+# # sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
+# # echo "naar:"
+# # jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # echo ""
+# # jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # mapeditorrootUrl=$(jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
-# # Replace the rootUrl with the one from the .env file
-# sed -i "s|$mapeditorrootUrl|$MAPEDITOR_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
-# echo "naar:"
-# jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# echo ""
-# jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# driverootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
-
-# # Replace the rootUrl with the one from the .env file
-# sed -i "s|$driverootUrl|$DRIVE_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
-# echo "naar:"
-# jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# echo ""
-# Set variables
+# # # Replace the rootUrl with the one from the .env file
+# # sed -i "s|$mapeditorrootUrl|$MAPEDITOR_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
+# # echo "naar:"
+# # jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # echo ""
+# # jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # driverootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 JSON_FILE="keycloak/esdl-mapeditor-realm.json"
 CLIENT_ID="essim-dashboard"
 ROOT_URL="https://dashboardtest.essim.online"
+# # Replace the rootUrl with the one from the .env file
+# sed -i "s|$driverootUrl|$DRIVE_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
+# echo "naar:"
+# jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json ' | .rootUrl = $GF_SERVER_ROOT_URL' \
+#   $JSON_FILE > tmp.json && mv tmp.json $JSON_FILE
 
-# Update rootUrl field using jq
-jq --arg client_id "$CLIENT_ID" --arg GF_SERVER_ROOT_URL "$ROOT_URL" \
-  '.clients[] | select(.id == $client_id) | .rootUrl = $GF_SERVER_ROOT_URL' \
+# echo ""
+# # Set variables
+
+
+# # # Update rootUrl field using jq
+# # jq --arg client_id "$CLIENT_ID" --arg GF_SERVER_ROOT_URL "$ROOT_URL" \
+# #   '.clients[] | select(.id == $client_id) | .rootUrl = $GF_SERVER_ROOT_URL' \
+# #   $JSON_FILE > tmp.json && mv tmp.json $JSON_FILE
+
+
+jq --arg client_id "$CLIENT_ID" --arg new_url "$ROOT_URL" \
+  '.clients[] | select(.id == $client_id) | .rootUrl = $new_url' \
   $JSON_FILE > tmp.json && mv tmp.json $JSON_FILE
-
-
-
