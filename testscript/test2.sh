7112cc2eb9be9aefbdf6570f167df2bcd8e923d0
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-30 11:50:33 +0200

    Update test2.sh

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -8,7 +8,7 @@ grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .r
 sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
 echo "naar:" 
 jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-
+echo ""
 jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
 mapeditorrootUrl=$(jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
@@ -16,7 +16,7 @@ mapeditorrootUrl=$(jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .
 sed -i "s|$mapeditorrootUrl|$MAPEDITOR_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
 echo "naar:"
 jq -r '.clients[] | select(.clientId == "esdl-mapeditor") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-
+echo ""
 jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
 driverootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
@@ -24,3 +24,4 @@ driverootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUr
 sed -i "s|$driverootUrl|$DRIVE_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
 echo "naar:"
 jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+echo ""
