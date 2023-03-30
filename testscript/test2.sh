a4c612450e44ac4057c711b4802086098671f860
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-30 10:38:22 +0200

    test2 update

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -2,7 +2,12 @@
 source .env
 # Read the rootUrl value from the JSON file
 jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+#rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
 # Replace the rootUrl with the one from the .env file
-sed -i "s|$rootUrl|$ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
+#sed -i "s|$rootUrl|$ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
+
+rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+
+# Replace the rootUrl value with the one from the .env file
+sed -i "s/\(\"rootUrl\" : \"\).*\(\",\)/\1${rootUrl}\2/" keycloak/esdl-mapeditor-realm.json
