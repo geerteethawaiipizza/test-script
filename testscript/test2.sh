46948e5979043cfa53a93ca722c8deecb2bdd3f1
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-30 14:50:46 +0200

    Update test2.sh

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 source .env
-JSON_FILE="keycloak/esdl-mapeditor-realm.json"
-CLIENT_ID="essim-dashboard"
-ROOT_URL="https://dashboardtest.essim.online"
-export $(grep -v '^#' .env | xargs)
+# JSON_FILE="keycloak/esdl-mapeditor-realm.json"
+# CLIENT_ID="essim-dashboard"
+# ROOT_URL="https://dashboardtest.essim.online"
+# export $(grep -v '^#' .env | xargs)
 
-# Read the rootUrl value from the JSON file
-# jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
-# grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+# # Read the rootUrl value from the JSON file
+# # jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+# # grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
-# # Replace the rootUrl with the one from the .env file
-# sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
-sed -i "/\"clientId\" : \"$CLIENT_ID\"/,/}/ s/\(\"rootUrl\" : \"\).*\(\",\)/\1$ROOT_URL\2/" $JSON_FILE
+# # # Replace the rootUrl with the one from the .env file
+# # sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
+# sed -i "/\"clientId\" : \"$CLIENT_ID\"/,/}/ s/\(\"rootUrl\" : \"\).*\(\",\)/\1$ROOT_URL\2/" $JSON_FILE
 
 
 # echo "naar:"
@@ -36,4 +36,11 @@ sed -i "/\"clientId\" : \"$CLIENT_ID\"/,/}/ s/\(\"rootUrl\" : \"\).*\(\",\)/\1$R
 
 # echo ""
 
+#!/bin/bash
 
+# Get the rootUrl value from the JSON file
+rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+
+# Replace the rootUrl value with the one from the .env file
+sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${GF_SERVER_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
+# sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
