362e3aa7eed83d22fa15d23146d3c24d8b30b228
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-31 14:40:10 +0200

    function test

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -3,18 +3,28 @@
 #Import env file
 source .env
 
-# Get the ESDLDRIVE rootUrl value from the JSON file
+DriveUrls () {
+    oldrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+    oldbaseUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .baseUrl' keycloak/esdl-mapeditor-realm.json)
+    oldredirectUris=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .redirectUris' keycloak/esdl-mapeditor-realm.json)
+    echo "Old value of ESDLDriverootUrl: $newrootUrl" 
 
-oldrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
-echo "Old value of ESDLDriverootUrl: $newrootUrl" 
-jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+    # Replace the Urls of ESDLdrive value with the one from the .env file
+    sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${DRIVE_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
+    sed -i "s#\(\"baseUrl\" : \"\).*\(\",\)#\1${DRIVE_BASE_URL}\2#" keycloak/esdl-mapeditor-realm.json
+    sed -i "s#\(\"redirectUris\" : \"\).*\(\",\)#\1${DRIVE_REDIRECT_URI}\2#" keycloak/esdl-mapeditor-realm.json
+    newrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+    echo "New value of ESDLDriverootUrl: $newrootUrl" 
+}
 
-# Replace the Urls of ESDLdrive value with the one from the .env file
+DriveUrls
 
-sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${GF_SERVER_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
-newrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
-echo "New value of ESDLDriverootUrl: $newrootUrl" 
 
+
+
+
+# redirectUris
+# baseUrl
 
 #sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
 
@@ -23,7 +33,7 @@ echo "New value of ESDLDriverootUrl: $newrootUrl"
 
 
 # # Read the rootUrl value from the JSON file
-jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
+#jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
 # # grafanarootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
 
 # # # Replace the rootUrl with the one from the .env file
