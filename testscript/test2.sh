1fe09aebcbdfc1215d3a993adabdc1eaaf5bc554
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-31 15:10:43 +0200

    Update test2.sh

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -6,7 +6,7 @@ source .env
 DriveUrls () {
     oldrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
     oldbaseUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .baseUrl' keycloak/esdl-mapeditor-realm.json)
-    redirectUris=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .redirectUris[0]' keycloak/clients.json)
+    redirectUris=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .redirectUris[0]' esdl-mapeditor-realm.json)
     echo "Old value of ESDLDriverootUrl: $oldrootUrl" 
     echo "Old value of ESDLDrivebaseUrl: $oldbaseUrl"
     echo "Old value of ESDLDriveredirectUris: $oldredirectUris"
@@ -14,11 +14,11 @@ DriveUrls () {
     sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${DRIVE_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
     sed -i "s#\(\"baseUrl\" : \"\).*\(\",\)#\1${DRIVE_BASE_URL}\2#" keycloak/esdl-mapeditor-realm.json
     # sed -i "s|\(\"redirectUris\" : \[\)\"https://drive.essim.online/\\*\"|\1\"${DRIVE_REDIRECT_URI}\"|" keycloak/client-list.json
-    sed -i "s|\"\(${redirectUris//\//\\/}\)\"|\"${DRIVE_REDIRECT_URI//\//\\/}\"|g" keycloak/clients.json
+    sed -i "s|\"\(${redirectUris//\//\\/}\)\"|\"${DRIVE_REDIRECT_URI//\//\\/}\"|g" esdl-mapeditor-realm.json
 
     newrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
     newbaseUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .baseUrl' keycloak/esdl-mapeditor-realm.json)
-    newredirectUris=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .redirectUris[0]' keycloak/clients.json)
+    newredirectUris=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .redirectUris[0]' esdl-mapeditor-realm.json)
     echo "New value of ESDLDriverootUrl: $newrootUrl"
     echo "Old value of ESDLDrivebaseUrl: $newbaseUrl"
     echo "Old value of ESDLDriveredirectUris: $newredirectUris"
