e49cb4dcb52f02a5ae6fc2f6c5887c29f4297028
geerteethawaiipizza <geertfischer@gmail.com>
2023-03-31 14:11:02 +0200

    Update test2.sh

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
+#Import env file
 source .env
+
+# Get the rootUrl value from the JSON file
+Echo "Old value:"
+rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
+
+# Replace the rootUrl value with the one from the .env file
+echo "New value:"
+sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${GF_SERVER_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
+
+
+
+#sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json 
+
+
+
+
 
 # # Read the rootUrl value from the JSON file
 # # jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json
@@ -9,11 +27,3 @@ source .env
 # # sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
 # sed -i "/\"clientId\" : \"$CLIENT_ID\"/,/}/ s/\(\"rootUrl\" : \"\).*\(\",\)/\1$ROOT_URL\2/" $JSON_FILE
 
-
-# Get the rootUrl value from the JSON file
-rootUrl=$(jq -r '.clients[] | select(.clientId == "essim-dashboard") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
-
-# Replace the rootUrl value with the one from the .env file
-echo "nieuw:"
-sed -i "s#\(\"rootUrl\" : \"\).*\(\",\)#\1${GF_SERVER_ROOT_URL}\2#" keycloak/esdl-mapeditor-realm.json
-# sed -i "s|$grafanarootUrl|$GF_SERVER_ROOT_URL|g" keycloak/esdl-mapeditor-realm.json
