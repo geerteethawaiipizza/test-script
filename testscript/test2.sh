e95a752e55b6b446b1032799ba493611cac59fea
geerteethawaiipizza <geertfischer@gmail.com>
2023-04-03 13:14:39 +0200

    test454

--- testscript/test2.sh
+++ testscript/test2.sh
@@ -15,7 +15,8 @@ DriveUrls () {
     sed -i "s#\(\"baseUrl\" : \"\).*\(\",\)#\1${DRIVE_BASE_URL}\2#" keycloak/esdl-mapeditor-realm.json
     # sed -i "s|\(\"redirectUris\" : \[\)\"https://drive.essim.online/\\*\"|\1\"${DRIVE_REDIRECT_URI}\"|" keycloak/client-list.json
     # sed -i "s|\"\(${oldredirectUris//\//\\/}\)\"|\"${DRIVE_REDIRECT_URI//\//\\/}\"|g" keycloak/esdl-mapeditor-realm.json
-    sed -i "s|\"${oldredirectUris//\//\\/}\"|\"${DRIVE_REDIRECT_URI//\//\\/}\"|g" keycloak/esdl-mapeditor-realm.json
+    # sed -i "s|\"${oldredirectUris//\//\\/}\"|\"${DRIVE_REDIRECT_URI//\//\\/}\"|g" keycloak/esdl-mapeditor-realm.json
+    sed -i "0,|\"\(${oldredirectUris//\//\\/}\)\"|{s||\"${DRIVE_REDIRECT_URI//\//\\/}\"|;}" keycloak/esdl-mapeditor-realm.json
 
 
     newrootUrl=$(jq -r '.clients[] | select(.clientId == "cdo-mondaine") | .rootUrl' keycloak/esdl-mapeditor-realm.json)
